f89bb2ee9edd60f78127b36bfa3a94220ed83a1e
Luis Sardon <lmsardon@gmail.com>
2016-04-06 20:31:33 +0000

    fix(index): commonjs to es6

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 'use strict';
 
-module.exports = {
-  Atom : require('./lib/components/Atom'),
-  Molecule : require('./lib/components/Molecule'),
-  Organism : require('./lib/components/Organism')
-};
+import Atom from './lib/components/Atom';
+import Molecule from './lib/components/Molecule';
+import Organism from './lib/components/Organism';
+
+export default Atom;
+export default Molecule;
+export default Organism;
+
