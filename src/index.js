1fee2e3aad07e2cd70e64d8a49b84db83da46b32
Luis Sardon <luis.sardon@orbis.com.pe>
2016-04-06 18:22:12 -0500

    wip(index): export components base class

--- src/index.js
+++ src/index.js
@@ -2,8 +2,4 @@ import Atom from './components/Atom';
 import Molecule from './components/Molecule';
 import Organism from './components/Organism';
 
-export default {
-  Atom : Atom,
-  Molecule : Molecule,
-  Organism : Organism
-};
+export default { Atom, Molecule, Organism };
