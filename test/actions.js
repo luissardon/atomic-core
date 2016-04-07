89e9dfe4aaccfa24dcc1f240087aa04ab42b7885
Luis Sardon <luis.sardon@orbis.com.pe>
2016-04-07 16:26:22 -0500

    feat(test): action listeners

--- test/actions.js
+++ test/actions.js
@@ -13,20 +13,47 @@ let atom = new Atom();
 let molecule = new Molecule();
 let organism = new Organism();
 
-test.cb('molecule action listener ACTIVATE should dispatch 2 listeners', t => {
+test.cb('ATOM SHOULD CALL ONE LISTENER', t => {
+  t.plan(1);
+
+  atom.addActionListener(Action.ACTIVATE, (a) => {
+    t.deepEqual(a.type, Action.ACTIVATE);
+    t.end();
+  });
+
+  atom.dispatchAction(Action.ACTIVATE);
+});
+
+test.cb('MOLECULE SHOULD CALL THE SECOND LISTENER BEFORE THE FIRST ONE', t => {
   t.plan(2);
 
   molecule.addActionListener(Action.ACTIVATE, (a) => {
-    console.log(a);
-    t.pass();
+    t.deepEqual(a.type, Action.ACTIVATE);
     t.end();
   });
 
   molecule.addActionListener(Action.ACTIVATE, (a) => {
-    console.log(a);
-    t.pass();
-    t.end();
-  },1);
+    t.deepEqual(a.type, Action.ACTIVATE);
+  }, 1);
 
   molecule.dispatchAction(Action.ACTIVATE);
+});
+
+test.cb('ORGANISM SHOULD CALL 3 LISTENERS BY CANCELING THE FIRST ONE AFTER HIS FIRST CALL', t => {
+  t.plan(3);
+
+  let times = 0;
+
+  organism.addActionListener(Action.ACTIVATE, (a) => {
+    t.deepEqual(a.type, Action.ACTIVATE);
+  }, 0, true);
+
+  organism.addActionListener(Action.ACTIVATE, (a) => {
+    t.deepEqual(a.type, Action.ACTIVATE);
+    if(times++ === 1)
+      t.end();
+  });
+
+  organism.dispatchAction(Action.ACTIVATE);
+  organism.dispatchAction(Action.ACTIVATE);
 });
