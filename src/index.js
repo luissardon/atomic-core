98639cb47e14f5fca4d55a8cb5807d184ca18fc8
Luis Sardon <luis.sardon@orbis.com.pe>
2016-04-07 14:46:07 -0500

    feat(index): export all classes

--- src/index.js
+++ src/index.js
@@ -3,11 +3,15 @@
 import _Atom from './components/Atom';
 import _Molecule from './components/Molecule';
 import _Organism from './components/Organism';
-import _UIComponent from './components/UIComponent';
-import _ActionDispatcher from './components/ActionDispatcher';
+import _UIComponent from './core/UIComponent';
+import _Action from './actions/Action';
+import _ActionDispatcher from './actions/ActionDispatcher';
+import _Utils from './utils/Utils';
 
 export let Atom = _Atom;
 export let Molecule = _Molecule;
 export let Organism = _Organism;
 export let UIComponent = _UIComponent;
+export let Action = _Action;
 export let ActionDispatcher = _ActionDispatcher;
+export let Utils = _Utils;
