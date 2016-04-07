93bd67db01ce99fcdfc9dee317db842bb07f4a31
Luis Sardon <luis.sardon@orbis.com.pe>
2016-04-07 14:48:06 -0500

    fix(test): extends

--- test/extends.js
+++ test/extends.js
@@ -10,6 +10,21 @@ import {
 
 import test from 'ava';
 
+let actionDispatcher = new ActionDispatcher();
+let uiComponent = new UIComponent();
+let atom = new Atom();
+let molecule = new Molecule();
+let organism = new Organism();
+
 test('UIComponent debe ser una extencion de ActionDispatcher', t => {
-  t.not(Atom, undefined);
+  t.truthy(uiComponent.actionFlow);
+});
+test('Atom debe ser una extencion de UIComponent', t => {
+  t.truthy(atom.render);
+});
+test('Molecule debe ser una extencion de UIComponent', t => {
+  t.truthy(molecule.render);
+});
+test('Organism debe ser una extencion de UIComponent', t => {
+  t.truthy(organism.render);
 });
