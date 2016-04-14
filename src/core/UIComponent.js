5d8f62658448cb76e74b474e91b6c8ee64397d66
Luis Sardon <lmsardon@gmail.com>
2016-04-14 04:32:08 -0500

    feat(UIComponent): type & cid definition

--- src/core/UIComponent.js
+++ src/core/UIComponent.js
@@ -7,19 +7,31 @@ import ActionDispatcher from '../actions/ActionDispatcher';
  * The UIComponent class is the base class for all atomic view components,
  * atoms, molecules and organisms.
  *
+ * @param {cid} Component cid
+ *
  */
 
 class UIComponent extends ActionDispatcher {
-  constructor(name) {
+  constructor(type, cid) {
     super();
 
     /**
-     * Define read-only 'name' property
+     * Define read-only 'type' property
      *
      */
 
-    Object.defineProperty(this, 'name', {
-      value: name,
+    Object.defineProperty(this, 'type', {
+      value: type,
+      writable: false
+    });
+
+    /**
+     * Define read-only 'cid' property
+     *
+     */
+
+    Object.defineProperty(this, 'cid', {
+      value: cid,
       writable: false
     });
 
