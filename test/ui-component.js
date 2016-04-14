c97958e9dc59d8d63a515b1755f1300eaf38ab7d
Luis Sardon <lmsardon@gmail.com>
2016-04-14 04:50:51 -0500

    fix(test): [ui-component] update name per cid

--- test/ui-component.js
+++ test/ui-component.js
@@ -5,9 +5,9 @@ import test from 'ava';
 
 let component = new UIComponent('some');
 
-test('SHOULD NOT CHANGE THE NAME OF THE COMPONENT PROPERTY', t => {
+test('SHOULDN\'T CHANGE THE CID OF THE COMPONENT PROPERTY', t => {
   try {
-    component.name = 'other';
+    component.cid = 'other';
     t.fail();
   } catch(e) {
     t.pass();
