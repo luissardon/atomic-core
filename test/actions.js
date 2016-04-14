92042a765aad90de6a4339c9c4a630c554590bbe
Luis Sardon <lmsardon@gmail.com>
2016-04-14 04:48:54 -0500

    feat(test): [actions] deactivate test

--- test/actions.js
+++ test/actions.js
@@ -13,47 +13,60 @@ let atom = new Atom();
 let molecule = new Molecule();
 let organism = new Organism();
 
-test.cb('COMPONENT SHOULD CALL ONE LISTENER', t => {
+test.cb('SHOULD CALL ONE LISTENER', t => {
   t.plan(1);
 
-  atom.addActionListener(Action.ACTIVATE, (a) => {
-    t.deepEqual(a.type, Action.ACTIVATE);
+  atom.addActionListener('TEST', (a) => {
+    t.deepEqual(a.type, 'TEST');
     t.end();
-  });
+  }, 0, true);
 
-  atom.dispatchAction(Action.ACTIVATE);
+  atom.dispatchAction('TEST');
 });
 
-test.cb('COMPONENT SHOULD CALL THE SECOND LISTENER BEFORE THE FIRST ONE', t => {
+test.cb('SHOULDN\'T CALL A LISTENER', t => {
+  t.plan(0);
+
+  atom.addActionListener('TEST', (a) => {
+    t.deepEqual(a.type, 'TEST');
+  });
+
+  atom.active = false;
+  atom.dispatchAction('TEST');
+
+  t.end();
+});
+
+test.cb('SHOULD CALL THE SECOND LISTENER BEFORE THE FIRST ONE', t => {
   t.plan(2);
 
-  molecule.addActionListener(Action.ACTIVATE, (a) => {
-    t.deepEqual(a.type, Action.ACTIVATE);
+  molecule.addActionListener('TEST', (a) => {
+    t.deepEqual(a.type, 'TEST');
     t.end();
   });
 
-  molecule.addActionListener(Action.ACTIVATE, (a) => {
-    t.deepEqual(a.type, Action.ACTIVATE);
+  molecule.addActionListener('TEST', (a) => {
+    t.deepEqual(a.type, 'TEST');
   }, 1);
 
-  molecule.dispatchAction(Action.ACTIVATE);
+  molecule.dispatchAction('TEST');
 });
 
-test.cb('COMPONENT SHOULD CALL 3 LISTENERS BY CANCELING THE FIRST ONE AFTER HIS FIRST CALL', t => {
+test.cb('SHOULD CALL 3 LISTENERS BY CANCELING THE FIRST ONE AFTER HIS FIRST CALL', t => {
   t.plan(3);
 
   let times = 0;
 
-  organism.addActionListener(Action.ACTIVATE, (a) => {
-    t.deepEqual(a.type, Action.ACTIVATE);
+  organism.addActionListener('TEST', (a) => {
+    t.deepEqual(a.type, 'TEST');
   }, 0, true);
 
-  organism.addActionListener(Action.ACTIVATE, (a) => {
-    t.deepEqual(a.type, Action.ACTIVATE);
+  organism.addActionListener('TEST', (a) => {
+    t.deepEqual(a.type, 'TEST');
     if(times++ === 1)
       t.end();
   });
 
-  organism.dispatchAction(Action.ACTIVATE);
-  organism.dispatchAction(Action.ACTIVATE);
+  organism.dispatchAction('TEST');
+  organism.dispatchAction('TEST');
 });
