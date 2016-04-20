7f7fb8c9afc7f710da9496fb27547a6e994b49c3
Luis Sardon <lmsardon@gmail.com>
2016-04-20 03:55:18 -0500

    feat(Utils): method to check if the value is a function

--- src/utils/Utils.js
+++ src/utils/Utils.js
@@ -6,7 +6,7 @@
  */
 
 class Utils {
-  
+
   /**
    * Generate a Unique ID
    *
@@ -15,6 +15,18 @@ class Utils {
 
   static getUID() {
     return Math.random().toString(36).substr(2, 9);
+  }
+
+  /**
+   * Check if the value is a Function type
+   *
+   * @param {value} Object
+   * @return {boolean} true if it matches
+   */
+  
+  static isFunction(value) {
+    let getType = {};
+    return value && getType.toString.call(value) === '[object Function]';
   }
 }
 
