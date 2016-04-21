c6da37c5c4545e397ed87d2e010ca5e496205c05
Luis Sardon <luis.sardon@orbis.com.pe>
2016-04-21 17:33:25 -0500

    style(index): remove spaces

--- src/index.js
+++ src/index.js
@@ -6,12 +6,10 @@ import _FormAction 				from './actions/FormAction';
 import _KeyboardAction 		from './actions/KeyboardAction';
 import _MouseAction 			from './actions/MouseAction';
 import _TouchAction 			from './actions/TouchAction';
-
 import _Atom 							from './components/Atom';
 import _Molecule 					from './components/Molecule';
 import _Organism 					from './components/Organism';
 import _ComponentObject 	from './components/ComponentObject';
-
 import _Utils 						from './utils/Utils';
 
 export let Action 					= _Action;
@@ -20,10 +18,8 @@ export let FormAction 			= _FormAction;
 export let KeyboardAction 	= _KeyboardAction;
 export let MouseAction 			= _MouseAction;
 export let TouchAction 			= _TouchAction;
-
 export let ComponentObject 	= _ComponentObject;
 export let Atom 						= _Atom;
 export let Molecule 				= _Molecule;
 export let Organism 				= _Organism;
-
 export let Utils 						= _Utils;
