cc25d654394caf94efcb23de1514297d58b539bb
Luis Sardon <luis.sardon@orbis.com.pe>
2016-04-21 19:07:00 -0500

    feat(test): [molecule] numChildren, addChild & getChildByName

--- test/molecule.js
+++ test/molecule.js
@@ -11,10 +11,10 @@ class neoAtom extends Atom {}
 class neoMolecule extends Molecule {}
 class neoOrganism extends Organism {}
 
-let atom = new neoAtom();
-let molecule = new neoMolecule();
-let molecule_2 = new neoMolecule();
-let organism = new neoOrganism();
+let atom = new neoAtom('myAtom');
+let molecule = new neoMolecule('myMolecule');
+let molecule_2 = new neoMolecule('myMolecule_2');
+let organism = new neoOrganism('myOrganism');
 
 test('SHOULD ADD AN ATOM INSTANCE TO A MOLECULE INSTANCE', t => {
 	t.deepEqual(molecule.addChild(atom), atom);
@@ -45,4 +45,17 @@ test('SHOULDN\'T ADD A MOLECULE INSTANCE TO ITSELF', t => {
   } catch (e) {
     t.deepEqual(e.message, 'You cannot add a child Molecule instance to itself');
   }
+});
+
+test('numChildren PROPERTY OF MOLECULE INSTANCE SHOULD BE 1', t => {
+  t.deepEqual(molecule.numChildren, 1);
+});
+
+test('SHOULD PASS ATOM INSTANCE FROM MOLECULE INSTANCE TO ANOTHER MOLECULE INSTANCE', t => {
+  molecule_2.addChild(atom);
+  t.deepEqual(atom.parent, molecule_2);
+});
+
+test('ATOM INSTANCE SHOULD BE IN MOLECULE_2', t => {
+  t.deepEqual(molecule_2.getChildByName('myAtom'), atom);
 });
